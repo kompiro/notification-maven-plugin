39ad8770e6d95d6ae90673e650f564ccdd1332a5
Hiroki Kondo <kompiro@gmail.com>
2012-08-15 16:09:14 +0900

    relese 0.8.0   - support simple notification build results window   - support to set duration value

--- src/main/java/org/kompiro/nortification/buildresult/NotificationMojo.java
+++ src/main/java/org/kompiro/nortification/buildresult/NotificationMojo.java
@@ -17,13 +17,16 @@ package org.kompiro.nortification.buildresult;
  */
 
 import java.awt.EventQueue;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-import org.apache.maven.execution.AbstractExecutionListener;
 import org.apache.maven.execution.BuildFailure;
 import org.apache.maven.execution.BuildSuccess;
 import org.apache.maven.execution.BuildSummary;
-import org.apache.maven.execution.ExecutionEvent;
+import org.apache.maven.execution.ExecutionListener;
+import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -34,12 +37,58 @@ import org.kompiro.nortification.ui.NotificationWindow;
 /**
  * Notify build result
  * 
+ * @author <a href="kompiro@gmail.com">Hiroki Kondo</a>
  * @goal notify
  * @phase initialize
  * @requiresProject
  * @description notify build result to user.
  */
 public class NotificationMojo extends AbstractMojo {
+
+	private final class NotifySessionEndedHandler implements
+			InvocationHandler {
+		private final ExecutionListener defaultExecutionListener;
+
+		private NotifySessionEndedHandler(
+				ExecutionListener defaultExecutionListener) {
+			this.defaultExecutionListener = defaultExecutionListener;
+		}
+
+		@Override
+		public Object invoke(Object target, Method method, Object[] args)
+				throws Throwable {
+			if(isExecutedSessionEnded(method)){
+				MavenProject project = session.getResult().getProject();
+				BuildSummary buildSummary = session.getResult()
+						.getBuildSummary(project);
+				if (buildSummary instanceof BuildSuccess) {
+					String title = "BUILD SUCCESS";
+					String message = String.format(
+							"Total Time : %ss", buildSummary.getTime());
+					showNotificationUI(title, message,NotificationType.SUCCESS);
+				} else if (buildSummary instanceof BuildFailure) {
+					BuildFailure failure = (BuildFailure) buildSummary;
+					String title = "BUILD FAILUERE!";
+					String message;
+					if(failure.getCause() != null){
+						message = String.format(
+								"Total Time : %ss\n%s", buildSummary.getTime(),
+								failure.getCause().getLocalizedMessage());
+					} else {
+						message = String.format(
+								"Total Time : %ss\n", buildSummary.getTime());
+					}
+					showNotificationUI(title, message,NotificationType.ERROR);
+				}
+			}
+			return method.invoke(defaultExecutionListener, args);
+		}
+
+		private boolean isExecutedSessionEnded(Method method) {
+			return method.getName().equals("sessionEnded") ;
+		}
+	}
+
 	/**
 	 * The Maven Session Object
 	 * 
@@ -48,29 +97,22 @@ public class NotificationMojo extends AbstractMojo {
 	 * @readonly
 	 */
 	protected MavenSession session;
+	
+    /**
+     * Notification duration
+     *
+     * @parameter expression="${maven.notification.duration}" default-value=2000
+     */
+    private Integer duration;
 
 	public void execute() throws MojoExecutionException {
-		session.getRequest().setExecutionListener(
-				new AbstractExecutionListener() {
-					@Override
-					public void sessionEnded(ExecutionEvent event) {
-						final MavenProject project = session.getResult().getProject();
-						BuildSummary buildSummary = session.getResult()
-								.getBuildSummary(project);
-						if (buildSummary instanceof BuildSuccess) {
-							String title = "Build Success!";
-							String message = String.format(
-									"Time : '%s's", buildSummary.getTime());
-							showNotificationUI(title, message,NotificationType.SUCCESS);
-						} else if (buildSummary instanceof BuildFailure) {
-							String title = "Build Failure!";
-							String message = String.format(
-									"Time : '%s's", buildSummary.getTime());
-							showNotificationUI(title, message,NotificationType.ERROR);
-							
-						}
-					}
-				});
+		MavenExecutionRequest request = session.getRequest();
+		final ExecutionListener defaultExecutionListener = request.getExecutionListener();
+		Object instance = Proxy.newProxyInstance(
+				this.getClass().getClassLoader(), 
+				new Class[]{ExecutionListener.class}, 
+				new NotifySessionEndedHandler(defaultExecutionListener));
+		request.setExecutionListener((ExecutionListener)instance);
 	}
 
 	private void showNotificationUI(final String title, final String message,final NotificationType type) {
@@ -81,7 +123,7 @@ public class NotificationMojo extends AbstractMojo {
 					window.setLocation(0, -10000); // hide initialization window
 					window.setTitle(title);
 					window.setMessage(message);
-					window.setDuration(2000);
+					window.setDuration(duration);
 					window.setType(type);
 					window.pack();
 					window.notifyUI();
@@ -92,8 +134,9 @@ public class NotificationMojo extends AbstractMojo {
 		} catch (InvocationTargetException e) {
 			getLog().error(e);
 		}
+		getLog().info(String.valueOf(duration));
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(duration);
 		} catch (InterruptedException e) {
 			getLog().error(e);
 		}
